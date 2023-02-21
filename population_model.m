e6bfadc7a3e5ab596e09b552fce3f7a0f301a3c2
LucyLi <125569779+OrionLucyLee@users.noreply.github.com>
2023-02-21 15:46:21 +0800

    Add files via upload
    matlab code for population chemotaxis model based on the intracellular pathway dynamics

---
+++ population_model.m
@@ -0,0 +1,83 @@
+% simulation for the steady cell profile of an E. coli population by means
+% of the population chemotaxis model based on the intercellular pathway
+% dynamics
+%% parameters for ligand-receptor bindings 
+KI1 = 18.2;		%Dissociation constant of MeAsp binding to inactive Tar, unit:uM
+KA1 = 3000;     %Dissociation constant of MeAsp binding to active Tar, unit:uM
+KI2 = 70;		%Dissociation constant of serine binding to inactive Tsr, unit:uM
+KA2 = 1000;     %Dissociation constant of serine binding to active Tsr, unit:uM
+KI3 = 1500;		%Dissociation constant of MeAsp binding to inactive Tsr, unit:uM
+KA3 = 3500;     %Dissociation constant of MeAsp binding to active Tsr, unit:uM
+N=6;            %receptor number in an effective cluster
+coff_ita=10;    
+
+%% attractant gradients
+dx=0.88;   x=(1:850)*dx;                      % channel length 
+attr=2000; L = (1-x/max(x))*attr/3 + attr/3;  % MeAsp gradient
+attr2=2.^(0:1:16);                            % loading concentration of serine
+
+%% parameters for lognormal distribution of Tar/Tsr
+mu = -0.843; 
+sigma = .97;  
+
+%% parameters for gamma distribution of Tar/Tsr
+% mu = 1.4; 
+% sigma = .5;  
+
+%% cell distribution of steady state;
+Rho=zeros(length(attr2),length(x));
+veff=zeros(length(attr2),length(x));
+
+for ii = 1:length(attr2)
+L2 = x/max(x)*attr2(ii)/3+attr2(ii)/3;        % serine gradient
+
+for N2=1:100
+ratio = N2/10;
+fL=(N* ratio./(1+ratio))*log((1+L/KI1) ./(1+L/KA1));
+fL2=(N./(1+ratio))*log((1+L2/KI2)./(1+L2/KA2));
+fL3=(N* ratio./(1+ratio))*log((1+L2/KI3)./(1+L2/KA3));
+fL_Total=fL+fL2+fL3;
+rho=exp(coff_ita*fL_Total);
+Rho_temp=rho/sum(rho);
+P2=lognpdf(ratio,mu,sigma);               %% lognormal 
+% P2=gampdf(ratio,mu,sigma);              %% gamma
+Rho(ii,:)=Rho(ii,:)+P2*Rho_temp;
+
+end
+Rho(ii,:) = Rho(ii,:)./sum(Rho(ii,:));
+
+end
+
+%% plot heat map
+attr2=[attr2,attr2(end)];
+Rho = [Rho;Rho(end,:)];
+
+figure('color','w');
+surf(x,attr2,Rho); shading flat; view(2);
+xlabel('x (\mum)'); xlim([0,740]); xticks([0,200,400,600]);
+ylabel('Serine Conc. (\muM)'); ylim([4,64000]); yticks([10,100,1000,10000]);
+set(gcf,'unit','centimeters','position',[10 10 8 7]);
+set(gca,'yscale','log','FontName','Arial','FontSize',9);
+set(gca,'Position',[0.14,0.15,0.7,0.75]);
+colormap jet;  h=colorbar;  caxis([0,0.01]);
+ax=gca; axpos=ax.Position; h.Position(3)=.7*h.Position(3); h.Position(1)=0.86;
+ax.Position=axpos; h.Ticks=0:0.002:0.01; h.Ruler.Exponent=-3;
+
+figure('color','w');  plot(x,Rho','linewidth',2); ylim([0 0.005]);  xlim([0 740]); xlabel('x (\mum)'); ylabel('Pdf');  
+
+
+%% calculate w1,w2
+x=x';
+Me=mean(Rho(1:end-1,:),2); 
+for i=1:size(Rho,1)-1
+    ind=find(Rho(i,:)>Me(i));
+    Ex1(i)=sum(x(ind).*Rho(i,ind)')/max(x)./sum(Rho(i,ind));
+    Ex2(i)=sqrt(sum((x(ind)-max(x)/2).^2.*(Rho(i,ind)'-Me(i)))./max(x).^2); 
+end
+
+figure('color','w'); subplot(2,1,1); semilogx(attr2(3:end-2),Ex1(3:end-1),'ob'); 
+line(2.^(2:0.2:14),[0.3*ones(1,61);0.7*ones(1,61)]); 
+ylim([0,1]); xlim([3,32000]); ylabel('w_1');
+hold on; subplot(2,1,2);semilogx(attr2(3:end-2),Ex2(3:end-1),'or'); 
+line(2.^(2:0.2:14),0.15*ones(1,61)); ylim([0,0.5]);  ylabel('w_2'); 
+xlim([3,32000]); xlabel('Serine conc. (\muM)');
